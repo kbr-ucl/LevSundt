81ba6a0fdecee4a0bb5ea0d8ba167c3ed92657b1
KBR <kbr@ucl.dk>
2022-09-14 13:36:55 +0200

    Bmi Domain oprettet

---
+++ LevSundt.Bmi.Domain.Test/BmiEntityTests/BmiEntityCreateTests.cs
@@ -0,0 +1,84 @@
+﻿using LevSundt.Bmi.Domain.Model;
+
+namespace LevSundt.Bmi.Domain.Test.BmiEntityTests;
+
+public class BmiEntityCreateTests
+{
+    /// <summary>
+    ///     Acceptabel højde er [100; 250]
+    /// </summary>
+    [Theory]
+    [InlineData(200)]
+    [InlineData(250)]
+    [InlineData(100)]
+    public void Given_Height_Is_Valid__Then_BmiEntity_Is_Created(double height)
+    {
+        // Arrange
+
+        // Act
+        var sut = new BmiEntity(height, 100);
+        // Assert
+    }
+
+    /// <summary>
+    ///     Acceptabel højde er [100; 250]
+    /// </summary>
+    [Theory]
+    [InlineData(250.01)]
+    [InlineData(99.99)]
+    public void Given_Height_Is_InValid__Then_ArgumentException_Is_Thrown(double height)
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+        Assert.Throws<ArgumentException>(() => new BmiEntity(height, 100));
+    }
+
+    /// <summary>
+    ///     Acceptabel vægt et [40,0; 250,0]
+    /// </summary>
+    [Theory]
+    [InlineData(100)]
+    [InlineData(250)]
+    [InlineData(40)]
+    public void Given_Weight_Is_Valid__Then_BmiEntity_Is_Created(double weight)
+    {
+        // Arrange
+
+        // Act
+        var sut = new BmiEntity(200, weight);
+        // Assert
+    }
+
+    /// <summary>
+    ///     Acceptabel vægt et [40,0; 250,0]
+    /// </summary>
+    [Theory]
+    [InlineData(250.01)]
+    [InlineData(39.99)]
+    public void Given_Weight_Is_InValid__Then_ArgumentException_Is_Thrown(double weight)
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+        Assert.Throws<ArgumentException>(() => new BmiEntity(200, weight));
+    }
+
+    [Theory]
+    [InlineData(200, 100, 25)]
+    [InlineData(190, 90, 24.9)]
+    public void Given_Hight_And_Weight__The_Bmi_Is_Calculatet_Correct(double height, double wight, double exptected)
+    {
+        // Arrange
+        
+        // Act
+        var sut = new BmiEntity(height, wight);
+
+        // Assert
+        Assert.Equal(exptected, Math.Round(sut.Bmi,1));
+    }
+}
---
+++ LevSundt.Bmi.Domain/Model/BmiEntity.cs
@@ -0,0 +1,39 @@
+﻿namespace LevSundt.Bmi.Domain.Model;
+
+public class BmiEntity
+{
+    public BmiEntity(double height, double weight)
+    {
+        // Check pre-condition
+        Height = height;
+        Weight = weight;
+
+        if (!IsValid()) throw new ArgumentException("Pre-conditions er ikke overholdt");
+
+        CalculateBmi();
+    }
+
+    public double Height { get; }
+    public double Weight { get; }
+    public double Bmi { get; private set; }
+
+    /// <summary>
+    /// Acceptabel højde er [100; 250]
+    //  Acceptabel vægt et [40,0; 250,0]
+    /// </summary>
+    /// <returns></returns>
+    protected bool IsValid()
+    {
+        if (Height < 100) return false;
+        if (Height > 250) return false;
+        if (Weight < 40) return false;
+        if (Weight > 250) return false;
+
+        return true;
+    }
+
+    protected void CalculateBmi()
+    {
+        Bmi = Weight / (Height/100 * Height/100);
+    }
+}
